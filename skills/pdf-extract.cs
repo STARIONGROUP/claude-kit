c8dddef6a1c802af1bd49ddad44c804f2c168888
samatstarion <s.gerene@stariongroup.eu>
2026-03-07 07:43:54 +0100

    [Fix] pdf-extract skill

--- skills/pdf-extract.cs
+++ skills/pdf-extract.cs
@@ -1,7 +1,26 @@
-#!/usr/bin/env dotnet-script
+// -------------------------------------------------------------------------------------------------
+//   <copyright file="pdf-extract.cs" company="Starion Group S.A.">
+//
+//     Copyright (c) 2026 Starion Group S.A.
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, softwareUseCases
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+//
+//   </copyright>
+//   ------------------------------------------------------------------------------------------------
+
 #:sdk Microsoft.NET.Sdk
-#:property TargetFramework net10.0
-#:package UglyToad.PdfPig@0.1.13
+#:property TargetFramework=net10.0
+#:package PdfPig@0.1.13
 
 using System;
 using System.IO;
